9dfe7b0a7e09d26e0a268868ae17414057822bf9
Aldana <AldanaBRZ@gmail.com>
2020-06-22 23:11:37 +0200

    First finished version

--- src/Components/NumberDisplay.js
+++ src/Components/NumberDisplay.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NumberDisplay = (props) => {
   return (
     <div className="col">
-      <h1>{props.number}</h1>
+      <h1 id="number-display">{props.number}</h1>
     </div>
   );
 };
